f763fa4a32d3e7a5d1b356234af8c6af999eb316
Lauren Gray <147671225+laurenjg@users.noreply.github.com>
2023-10-21 13:31:31 +0100

    Update db.py
    Added function to find a dog with a given criteria

--- PawsitiveAdoptions/db.py
+++ PawsitiveAdoptions/db.py
@@ -38,9 +38,60 @@ def get_shelter_info():
             print("DB connection is closed")
 
 
+
+
+# Finding a dog to match criteria
+def get_adoption_info(location, age, size, sex):
+    try:
+        db_name = "PawsitiveAdoptions"
+        db_connection = _connect_to_db(db_name)
+        cur = db_connection.cursor()
+        print("\nSucessfully connected to DB: %s" % db_name)
+        print(f"The dogs available for adoption that meet your criteria in {location} are:")
+
+        # commented out inputs from main.py... uncomment and delete arguments from get_adoption_info() in main() function to activate in console
+        #location = input("Where are you looking to adopt from? London or Belfast?")
+        #age = input("What age group of dog are you looking for? (Puppy, Adult, Senior): ")
+        #size = input("What size of dog are you looking for? (Small, Medium, Large): ")
+        #sex = input("What gender of dog are you looking for? (Male, Female): ")
+
+        if location == "London":
+            # shelter_id 1 = London
+            query = """SELECT r.dog_name, d.breed 
+            FROM shelter AS s RIGHT JOIN rescued_dogs AS r ON s.shelter_id = r.shelter_id 
+            RIGHT JOIN dog_details AS d ON r.details_id = d.details_id 
+            WHERE r.shelter_id = 1 AND d.age = "{}" AND d.size = "{}" AND d.sex = "{}";""".format(age, size, sex)
+        elif location == "Belfast":
+            # shelter_id 2 = Belfast
+            query = """SELECT r.dog_name, d.breed 
+            FROM shelter AS s RIGHT JOIN rescued_dogs AS r ON s.shelter_id = r.shelter_id 
+            RIGHT JOIN dog_details AS d ON r.details_id = d.details_id
+            WHERE r.shelter_id = 2 AND d.age = "{}" AND d.size = "{}" AND d.sex = "{}";""".format(age, size, sex)
+        else:
+            print("Invalid location. Please choose London or Belfast.")
+
+        cur.execute(query)
+        result = cur.fetchall()
+
+        for i in result:
+            print(i)
+        cur.close()
+
+    except Exception:
+        raise DbConnectionError("Failed to connect to the DB")
+
+    else:
+        if db_connection:
+            db_connection.close()
+            print("DB connection is closed")
+
+
+
 #calling functions 
 def main():
    get_shelter_info()
+    # calling function to find a dog that meets specific criteria
+   get_adoption_info("Belfast", "Adult (1-7 years)", "medium", "male")
 
 
 #defining main 
